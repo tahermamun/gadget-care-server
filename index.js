b96c4d872062cc848f8830c5eea8cd5dfadbac01
tahermamun <tahermamun5@gmail.com>
2021-04-18 23:57:04 +0600

    handle fix error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const ObjectID = require('mongodb').ObjectID;
 const MongoClient = require('Mongodb').MongoClient
 require('dotenv').config()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const app = express()
 app.use(bodyParser.json())
