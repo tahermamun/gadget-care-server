043bdc0a0837f3571d8a0f98eb471b6c35a752bc
tahermamun <tahermamun5@gmail.com>
2021-04-18 21:38:40 +0600

    error handle

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ require('dotenv').config()
 
 const app = express()
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 
 
@@ -132,6 +131,7 @@ client.connect(err => {
             })
 
     })
+
 
     app.post('/isAdmin', (req, res) => {
         const email = req.body.email
